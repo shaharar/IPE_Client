7a5df368fece24fc8c19c280a8e0dadb08e07900
shaharar <shaharar@post.bgu.ac.il>
2019-07-02 20:40:32 +0300

    filter categories, add rank

--- routing/Modules/POI/poiController.js
+++ routing/Modules/POI/poiController.js
@@ -11,7 +11,9 @@ angular.module("parisApp")
         self.starImg ="/images/greyStar.png";
         self.nameForSearch = "";
         self.isSearch = false;
-
+        self.chosenCategories = [];
+        self.rankOptions = [1,2,3,4,5];
+        self.getCategories();
     }
 
 
@@ -29,7 +31,6 @@ angular.module("parisApp")
     self.setPoisByCatLists=function(){
         for(var i=0; i<self.allPoiRes.length; i++){
             let poi = self.allPoiRes[i];
-            console.log(poi.CategoryID)
             if(poi.CategoryID == "1"){
                 self.poisByCat1.push(poi);
             }
@@ -44,6 +45,30 @@ angular.module("parisApp")
             }
         }
     }
+
+    self.getCategories=function(){  
+        httpRequests.get("POIs/getPOIsCategories")
+        .then (function (response){
+            self.categories = response.data;
+            
+            for (var i = 0; i < self.categories.length; i++){
+                self.chosenCategories.push(""+self.categories[i].ID)
+            }
+        }, function(response){
+            //------------TODO OPTIONAL handle error------------------------
+        });
+    }
+
+    
+    self.chooseCategories=function (category) {
+        if (self.chosenCategories[category] == -1) {
+            self.chosenCategories[category] = category;
+        }
+        else {
+            self.chosenCategories[category] = -1;
+        }
+    }
+
     self.getPOIsCategories=function(){
         httpRequests.get("POIs/getPOIsCategories")
         .then (function (response){
@@ -90,7 +115,6 @@ angular.module("parisApp")
             self.isSearch = true;
         }, function(response){
             alert("No POIs were found");
-            //------------TODO OPTIONAL handle error------------------------
        });
     }
 
@@ -100,10 +124,36 @@ angular.module("parisApp")
 
     self.return=function(){
         self.isSearch = false;
+        self.nameSearched = "";
     }
+
+    self.setIDAddRank=function(poiID){
+        self.idInAddRank = poiID;
+    }
+
+    self.addRank=function () {
+        var newReview = {
+            ID: self.idInAddRank,
+            Rank: self.rankInput,
+            Review: self.reviewInput
+        }
+        console.log(newReview)
+        httpRequests.post("POIs/private/addRank/", newReview)
+            .then(function (response) {
+                if (response.data.message == "Review added successfuly" || 
+                    response.data.message == "This POI already has this review from this user") {
+                    alert(response.data.message)
+                }
+            }, function (response) {
+                alert("Something went wrong..");
+                return;
+            });
+    }
+
+
+        //  TODO-----------add to favorites ???-------------------------------------
+
+
 }]);
 
-    //  TODO-----------add rank-------------------------------------
 
-
-    //  TODO-----------add to favorites ???-------------------------------------
